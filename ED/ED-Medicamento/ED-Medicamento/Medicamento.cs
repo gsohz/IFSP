4a626f20d16fb18ca91703218cc99dcc22bdc3d2
Gabriel Souza <98726404+gsohz@users.noreply.github.com>
2022-11-15 01:13:16 -0300

    Add files via upload

---
+++ ED/ED-Medicamento/ED-Medicamento/Medicamento.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ED_Medicamento
+{
+    class Medicamento
+    {
+        private int id;
+        private string nome;
+        private string laboratorio;
+        private Queue<Lote> lotes;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Laboratorio { get => laboratorio; set => laboratorio = value; }
+        internal Queue<Lote> Lotes { get => lotes; set => lotes = value; }
+
+        public Medicamento()
+        {
+            Id = -1;
+        }
+
+        public Medicamento(int id, string nome, string laboratorio)
+        {
+            Id = id;
+            Nome = nome;
+            Laboratorio = laboratorio;
+            Lotes = new Queue<Lote>();
+        }
+
+        public int qtdeDisponivel()
+        {
+            int total = 0;
+            //DateTime hoje = DateTime.Now;
+            foreach(Lote l in Lotes)
+            {
+                total += l.Qtde;
+                /*
+                if(l.Venc.Year > hoje.Year)
+                {
+                    total += l.Qtde;
+                } else if (l.Venc.Year == hoje.Year && l.Venc.Month >= hoje.Month && l.Venc.Day > hoje.Day)
+                {
+                    total += l.Qtde;
+                }
+                */
+            }
+            return total;
+        }
+
+        public void comprar(Lote lote)
+        {
+            Lotes.Enqueue(lote);
+        }
+
+        public bool vender(int qtde)
+        {
+            int qtdDisponivel = qtdeDisponivel();
+            if (qtdDisponivel == 0 || qtdDisponivel < qtde)
+            {
+                return false;
+            }
+
+            int aux = qtde;
+            int totalLote;
+
+            foreach (Lote l in Lotes)
+            {
+                while(aux != 0)
+                {
+                    aux -= l.Qtde;
+                    totalLote = l.Qtde - qtde;
+                    if(totalLote <= 0)
+                    {
+                        Lotes.Dequeue();
+                    }
+                }
+            }
+
+            bool medicamentosVendidos = (qtdDisponivel - qtde == 0);
+            return medicamentosVendidos;
+        }
+
+        public string toString()
+        {
+            return Id + "-" + Nome + "-" + Laboratorio + "-" + qtdeDisponivel();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Id.Equals(((Medicamento)obj).Id);
+        }
+    }
+}
---
+++ ED/ED-Medicamento/ED-Medicamento/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ED_Medicamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key;
+            Medicamentos medicamentos = new Medicamentos();
+
+            do
+            {
+                Console.WriteLine(
+                  "\n--------------------------------------------------------------------" +
+                  "\n| 0. Finalizar processo                                            |" +
+                  "\n| 1. Cadastrar medicamento                                         |" +
+                  "\n| 2. Consultar medicamento (sintético: informar dados)            |" +
+                  "\n| 3. Consultar medicamento (analítico: informar dados + lotes)   |" +
+                  "\n| 4. Comprar medicamento (cadastrar lote)                          |" +
+                  "\n| 5. Vender medicamento (abater do lote mais antigo)               |" +
+                  "\n| 6. Listar medicamentos (informando dados sintéticos)             |" +
+                  "\n--------------------------------------------------------------------");
+
+                Console.Write("\n\nEscolha uma opção: ");
+                key = int.Parse(Console.ReadLine());
+                Console.Write("\n\n");
+
+                switch (key)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                }
+            } while (key != 0);
+        }
+    }
+}
